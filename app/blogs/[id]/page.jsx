9f512f2ff549296615a60e483ce0d70833f98296
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-03-25 17:34:09 +0200

    added routing

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,7 @@
+const BlogDetail = ({ params }) => {
+  const id = params?.id;
+
+  return <div>BlogDetail id: {id}</div>;
+};
+
+export default BlogDetail;
---
+++ app/ui/Button.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+const Button = ({ label, color }) => {
+  return (
+    <button
+      type="submit"
+      style={{ color: color }}
+      className={`bg-${color}-600 text-gray-400 border-2 py-2 px-4 rounded-md hover:bg-${color}-600 focus:outline-none focus:ring focus:border-${color}-300`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;

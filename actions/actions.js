92c42efc58bc214cd5d96656a4e21f6977e40866
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-03-27 14:06:56 +0200

    added add blog form/action addBlog

---
+++ actions/actions.js
@@ -0,0 +1,28 @@
+'use server';
+
+import { PrismaClient } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const prisma = new PrismaClient();
+
+export const addBlog = async formData => {
+  //collect info from form using formData
+  const title = formData.get('title');
+  const category = formData.get('category');
+  const description = formData.get('description');
+  const imageUrl = formData.get('imageUrl');
+
+  //push the data into the DB
+  const new_blog = await prisma.blog.create({
+    data: {
+      imageUrl: imageUrl ? imageUrl : null,
+      title,
+      category,
+      description,
+    },
+  });
+
+  //   revalidatePath('/blogs/add-blog');
+  redirect('/blogs');
+};
--- app/blogs/add-blog/page.jsx
+++ app/blogs/add-blog/page.jsx
@@ -1,5 +1,12 @@
+import AddBlogRorm from '../../components/forms/AddBlogForm';
+
 const AddBlog = () => {
-  return <div>AddBlog</div>;
+  return (
+    <div>
+      <h2 className="text-center text-2xl mt-4 px-2 py-2 font-bold">Add Blog Page</h2>
+      <AddBlogRorm />
+    </div>
+  );
 };
 
 export default AddBlog;
